c2a81664e6c08c4c0aa7ca1591c1f13132b23833
Shervin Shaikh <shervin.shaikh@citrix.com>
2014-08-15 14:02:21 -0700

    Basic implmentation of sending a Yo when participant arrives in G2M Free room

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+console.log('Content script!');
+
+var username = null;
+checkForParticipants();
+
+chrome.storage.sync.get('yoUsername', function(data){
+  // console.log("GET:", data.yoUsername);
+  username = data.yoUsername;
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  console.log("Changes", changes);
+  if(changes['yoUsername'] !== null){
+    username = changes['yoUsername'].newValue;
+    console.warn("Username changed from:", changes['yoUsername'].oldValue + ", to:", username);
+  }
+});
+
+function checkForParticipants(){
+  var numParticipants = document.getElementsByClassName('participants').length;
+  if(numParticipants === 0){
+    setTimeout(checkForParticipants, 100);
+  } else {
+    console.log("Someone joined! Sending a YO to", username);
+
+    //curl --data "api_token=b9592bb7-8505-d615-160b-a2dcbf818449&username=sherv8" http://api.justyo.co/yo/
+    var request = new XMLHttpRequest();
+    request.open("POST", 'http://api.justyo.co/yo/', false);
+    request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    request.send('api_token=b9592bb7-8505-d615-160b-a2dcbf818449&username=' + username);
+    console.log(request.responseText);
+  }
+}
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+var submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', saveChanges);
+
+chrome.storage.sync.get('yoUsername', function(data){
+  console.log("GET:", data.yoUsername);
+  document.getElementById('currentUser').innerHTML = data.yoUsername;
+});
+
+function saveChanges() {
+  console.log('saving...');
+  // Get a value saved in a form.
+  var yoUsername = document.getElementById('yoUsername').value.toUpperCase();
+  // Check that there's some code there.
+  if (!yoUsername) {
+    message('Error: No value specified');
+    return;
+  }
+  // Save it using the Chrome extension storage API.
+  chrome.storage.sync.set({'yoUsername': yoUsername}, function() {
+    console.log('SET:', {'yoUsername': yoUsername});
+    document.getElementById('currentUser').innerHTML = yoUsername;
+    // Notify that we saved.
+    message('Settings saved');
+  });
+}
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  console.log("Changes", changes);
+  if(changes['yoUsername'] !== null){
+    document.getElementById('currentUser').innerHTML = changes['yoUsername'].newValue;
+    console.warn("Username changed from:", changes['yoUsername'].oldValue + ", to:", changes['yoUsername'].newValue);
+  }
+});
